d06dfa3ac0a10f0a5c2e9272c88a13495ecfac6d
des <dessanndes@gmail.com>
2017-09-03 20:20:33 +0300

    #1 create setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# python setup.py sdist register bdist_egg upload
+from setuptools import setup, find_packages
+
+setup(
+    name='notify-client',
+    version='0.0.1',
+    description='Soft Way company notify service client.',
+    author='Telminov Sergey',
+    author_email='sergey@telminov.ru',
+    url='https://github.com/telminov/notify-client',
+    include_package_data=True,
+    packages=find_packages(),
+    license='The MIT License',
+    install_requires=[
+        'requests',
+    ],
+)
